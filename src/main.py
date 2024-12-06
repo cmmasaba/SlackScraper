94ae35fb64f5f313d9c1167237d8a8ec415846d3
cmmasaba <mmasabacollins9@gmail.com>
2024-12-06 15:58:56 +0300

    Driver code

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from slack_bot.slack_elt_automation import SlackScraper
 
 def main():
     save_to_cloud_storage = True
-    app = SlackScraper(saving_to_cloud=save_to_cloud_storage)
+    app = SlackScraper(save_to_cloud=save_to_cloud_storage)
 
     print('Starting automation...', end='\n')
     app.start()
