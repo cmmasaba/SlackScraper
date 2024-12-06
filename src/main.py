3a475d7ce5aab94a12af81c826edfbf2b4a088e3
cmmasaba <mmasabacollins9@gmail.com>
2024-12-06 15:20:45 +0300

    Driver code

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
 # TODO: implement the entrypoint
+from slack_bot.slack_elt_automation import SlackScraper
+
+def main():
+    saving_to_cloud_storage = True
+    app = SlackScraper(saving_to_cloud=saving_to_cloud_storage)
+
+    print('Starting automation...', end='\n')
+    app.start()
+    print('Finished successfully.')
